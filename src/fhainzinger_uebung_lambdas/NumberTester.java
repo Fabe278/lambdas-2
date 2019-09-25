384e3ecb2d165c5e84ecc41eb4515f972c3186e1
fhainzinger17 <fabian.irger3@gmail.com>
2019-09-25 18:16:59 +0200

    NumberTester created with functions

---
+++ src/fhainzinger_uebung_lambdas/NumberTester.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fhainzinger_uebung_lambdas;
+
+/**
+ *
+ * @author fabia
+ */
+public class NumberTester {
+    public NumberTester(String fileName){}
+    
+    public void setOddEventTester(NumberTest oddTester) {}
+    
+    public void setPrimeTester(NumberTest primeTester) {}
+    public void setPalindromeTester(NumberTest palindromeTester) {}
+    
+    public void testFile() {}
+}
