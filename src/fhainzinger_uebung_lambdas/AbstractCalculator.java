c7b3c27222e47a35f6ac1080874d587fbf5f6f1b
fhainzinger17 <fabian.irger3@gmail.com>
2019-09-28 11:57:09 +0200

    created Calculator, functions missing

--- src/fhainzinger_uebung_lambdas/AbstractCalculator.java
+++ src/fhainzinger_uebung_lambdas/AbstractCalculator.java
@@ -19,7 +19,7 @@ public abstract class AbstractCalculator {
         this.add = add; this.subtract = subtract; this.multiply = multiply; this.divide = divide;
     }
     
-    public abstract Number ad(Number a, Number b);
+    public abstract Number add(Number a, Number b);
     public abstract Number subtract (Number a, Number b);
     public abstract Number multiply (Number a, Number b);
     public abstract Number divide (Number a, Number b);
--- src/fhainzinger_uebung_lambdas/RationalCalculator.java
+++ src/fhainzinger_uebung_lambdas/RationalCalculator.java
@@ -9,6 +9,31 @@ package fhainzinger_uebung_lambdas;
  *
  * @author fabia
  */
-public class RationalCalculator {
+public class RationalCalculator extends AbstractCalculator{
+
+    public RationalCalculator(CalculationOperation add, CalculationOperation subtract, CalculationOperation multiply, CalculationOperation divide) {
+        super(add, subtract, multiply, divide);
+    }
+
+    @Override
+    public Number add(Number a, Number b) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Number subtract(Number a, Number b) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Number multiply(Number a, Number b) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Number divide(Number a, Number b) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
     
 }
